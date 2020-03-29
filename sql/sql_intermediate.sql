1fb6e261ea12f4c819d88db763da8cd6978fbc1e
Victor George <victorgeorge@Victors-MacBook-Pro.local>
2020-03-29 19:54:20 +0200

    with that Gold is bagged. Taking a break from Hackerrank

--- sql/sql_intermediate.sql
+++ sql/sql_intermediate.sql
@@ -61,10 +61,122 @@ Enter your query here.
 select ceil(avg(Salary) - avg(replace(Salary, '0', '')))
 from EMPLOYEES;
 
+/*
+limit, logic of sorting by desc and taking top most row
+*/
+
+select round(LONG_W, 4)
+from STATION
+where LAT_N < 137.2345
+order by LAT_N desc
+limit 1;
+
+/*
+Enter your query here.
+*/
+select round(LAT_N, 4)
+from STATION
+where LAT_N > 38.7780
+order by LAT_N asc
+limit 1;
+
+/*
+Enter your query here.
+*/
+select round(LONG_W, 4)
+from STATION
+where LAT_N > 38.7780
+order by LAT_N asc
+limit 1;
+
+/*
+Enter your query here.
+*/
+select round(max(LAT_N) + max(LONG_W) - min(LAT_N) - min(LONG_W), 4)
+from STATION;
+
+/*
+Enter your query here.
+*/
+select round(sqrt(power(max(LAT_N) - min(LAT_N), 2) + power(max(LONG_W)  - min(LONG_W), 2)), 4)
+from STATION;
+
+set @row_index := -1;
+select round(avg(stat.lat_n), 4)
+from 
+(select @row_index := @row_index +1 as row_index,
+STATION.LAT_N as lat_n
+from STATION
+order by STATION.LAT_N) as stat
+where stat.row_index in(floor(@row_index/2), ceil(@row_index/2));
+
+/*
+joins, finally.
+*/
+select sum(t.population)
+from 
+(select CITY.POPULATION as population, 
+	CITY.COUNTRYCODE as country_code, 
+	COUNTRY.CONTINENT as continent
+from CITY
+left join COUNTRY
+on CITY.COUNTRYCODE = COUNTRY.CODE) as t
+where t.continent = 'Asia';
+
+/*
+joins, finally.
+*/
+select (t.city)
+from
+(select CITY.NAME as city, COUNTRY.CONTINENT as continent
+from CITY
+inner join
+COUNTRY
+on CITY.COUNTRYCODE = COUNTRY.CODE) as t
+group by t.continent = 'Africa';
 
 
+/*
+average population of each continent
+*/
+select t.continent, floor(avg(t.population))
+from
+(select COUNTRY.CONTINENT as continent, CITY.POPULATION as population, 
+CITY.COUNTRYCODE as country_code
+from COUNTRY
+inner join
+CITY
+on COUNTRY.CODE = CITY.COUNTRYCODE) as t
+group by t.continent;
 
+/*
+basic join/the-report
+uses on - between and case, when --- then 
+*/
+select case 
+when t.grade < 8 then 'NULL'
+else t.name end, 
+t.grade, t.marks from
+(select Students.Name as name, 
+Grades.Grade as grade,
+Students.Marks as marks
+from Students inner join Grades
+on marks between Grades.Min_Mark and Grades.Max_mark) as t
+order by t.grade desc, t.name;
 
+/*
+basic join/full-score
+uses on - between and case, when --- then 
+*/
+select Hackers.hacker_id as h_id, Hackers.name as h_name,
+Challenges.challenge_id as c_id, Challenges.difficulty_level as level,
+Difficulty.score as max_score
+from 
+Hackers
+left join Challenges
+on Hackers.hacker_id = Challenges.hacker_id
+inner join Difficulty
+on Challenges.difficulty_level = Difficulty.difficulty_level;
 
 
 
