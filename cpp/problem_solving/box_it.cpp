141b0b05643d8943dd1a8f756ff38ee5476048fe
victorvg17 <victorvg17@gmail.com>
2020-11-15 00:56:23 +0100

    fixed the failing case by making variables long

--- cpp/problem_solving/box_it.cpp
+++ cpp/problem_solving/box_it.cpp
@@ -1,78 +1,112 @@
-// #include<bits/stdc++.h>
-#include<stdbool.h>
-#include<iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
-//Implement the class Box
-//l,b,h are integers representing the dimensions of the box
-// The class should have the following functions :
-class Box{
-    private:
-        int length;
-        int breadth;
-        int height;
 
-    public:
-        //constructors
-        // Box();
-        Box():length(0),
-              breadth(0),
-              height(0){}
-        // Box(int,int,int);
-        Box(int l, int b, int h):length(l),
-                                breadth(b),
-                                height(h){}
-        // Box(Box);
-        Box(Box& B){
-          length = B.getLength();
-          breadth = B.getBreadth();
-          height = B.getHeight();
-        }
-        // Return box's length
-        int getLength() const{
-          return length;
-        }
-        // Return box's breadth
-        int getBreadth() const{
-          return breadth;
-        }
-        //Return box's height
-        int getHeight() const{
-          return height;
-        }
-        // Return the volume of the box
-        long long CalculateVolume() const{
-          return length*breadth*height;
-        }
-        //Overload operator < as specified
-        //bool operator<(Box& b)
-        bool operator<(const Box& b){
-          if (length < b.getLength()){
-            return true;
-          }
-          else if (length == b.getLength() && breadth < b.getBreadth()){
-            return true;
-          }
-          else if (length == b.getLength() && breadth == b.getBreadth() && height < b.getHeight()) {
-            return true;
-          }
-          return false;
-        }
+class Box {
+public:
+	Box():length(0),
+				breadth(0),
+				height(0){}
+	Box(int l, int b, int h):length(l),
+													breadth(b),
+													height(h){}
+	Box(Box& B){
+		length = B.getLength();
+		breadth = B.getBreadth();
+		height = B.getHeight();
+	}
 
-        //declare os as friend
-        friend ostream& operator<<(ostream& out, const Box& B);
+	long getLength();
+	long getBreadth();
+	long getHeight();
+	long CalculateVolume();
+
+	bool operator<(const Box& b2) const;
+	friend ostream& operator<<(ostream& os, Box& b);
+
+private:
+	long length;
+	long breadth;
+	long height;
 };
 
-//Overload operator << as specified
-ostream& operator<<(ostream& out, const Box& B){
-  out<<B.getLength()<<" "<<B.getBreadth()<<" "<<B.getHeight()<<endl;
-  return out;
+long Box::getLength() {
+	return length;
+}
+long Box::getBreadth() {
+	return breadth;
+}
+long Box::getHeight() {
+	return height;
+}
+unsigned long long int Box::CalculateVolume() {
+	return length*breadth*height;
+}
+bool Box::operator<(const Box& b2) const {
+	if (length < b2.length) {
+		return true;
+	} else if (length == b2.length && breadth < b2.breadth){
+		return true;
+	} else if (length == b2.length && breadth == b2.breadth && height < b2.height){
+		return true;
+	} else {
+		return false;
+	}
+}
+ostream& operator<<(ostream& os, Box& b) {
+	os << b.getLength() << " " << b.getBreadth() << " " << b.getHeight();
+	return os;
 }
 
-int main(void){
-  Box b(8, 6, 5);
-  Box c(16, 6, 5);
-  cout<<b;
-  if (b < c){
-    cout << "b is smaller" << endl;
-  }
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
 }
